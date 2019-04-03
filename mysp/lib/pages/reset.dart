fa7a790981bf578eab48c4b67889e8f4afd29a75
Mac <mac@MacdeMac-mini.local>
2019-04-03 17:59:00 +0800

    fix

--- mysp/lib/pages/reset.dart
+++ mysp/lib/pages/reset.dart
@@ -9,21 +9,78 @@ class _PasswordPageState extends State<PasswordPage> {
   TextEditingController _phone = new TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return new SingleChildScrollView(
-      child: new Column(
-        children: <Widget>[
-          Container(
-            child: Column(
-              children: <Widget>[
-                new Row(
-                  children: <Widget>[
-                    
-                  ],
-                )
-              ],
+    return Scaffold(
+      appBar: new AppBar(
+        backgroundColor: Colors.white,
+        title: new Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            new RaisedButton(
+              child: new Text('back'),
+              onPressed: (){
+                Navigator.pop(context);
+              },
             ),
-          )
-        ],
+            new Align(
+              alignment: Alignment.center,
+              child: new Text(
+                '找回密码',
+                style: new TextStyle(
+                  color: Colors.black
+                ),
+              )
+            )
+          ],
+        )
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.all(20.0),
+              margin: EdgeInsets.only(top: 20.0),
+              color: Colors.white,
+              child: Column(
+                children: <Widget>[
+                  new Row(
+                    children: <Widget>[
+                      Expanded(
+                        flex: 1, 
+                        child:TextField(
+                          controller: _phone,
+                          decoration: new InputDecoration.collapsed(
+                            hintText:  '请输入姓名',
+                          ),
+                        )
+                      )
+                    ],
+                  ),
+                  Divider(),
+                  new Row(
+                    children: <Widget>[
+                      Expanded(
+                        flex: 1, 
+                        child:TextField(
+                          controller: _phone,
+                          decoration: new InputDecoration.collapsed(
+                            hintText:  '请输入验证码',
+                          ),
+                        )
+                      ),
+                      new Container(
+                        decoration: BoxDecoration(
+                          // borderColor: Colors.red,
+                          border: Border(top: BorderSide.none)
+                        ),
+                        child: new Text('获取验证码'),
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
       )
     );
   }
