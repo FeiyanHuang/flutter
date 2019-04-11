a3d63669c49d00200f80eb5782232a3d1d89abd0
Mac <mac@MacdeMac-mini.local>
2019-04-11 17:59:57 +0800

    mapDemo

---
+++ mapdemo/lib/map.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Map extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Text('map')
+    );
+  }
+}
